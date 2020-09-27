53f6d347afa0983b5d72700597650e119a132a72
hidayat26 <mdhidayat9163@gmail.com>
2020-09-27 22:16:36 +0530

    Updates

---
+++ precache-manifest.670712ce77c6fde006c7cdd32afc4e1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee9ee85cb6df7ec62a3892f71bb9fca",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "7f397668c6e809d346c9",
+    "url": "/tic-tac-toe/static/css/main.fc9d53d9.chunk.css"
+  },
+  {
+    "revision": "d3389eb0ec529da3c9b5",
+    "url": "/tic-tac-toe/static/js/2.05fb6b8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.05fb6b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f397668c6e809d346c9",
+    "url": "/tic-tac-toe/static/js/main.bec428de.chunk.js"
+  },
+  {
+    "revision": "61109fc60463736a7ada",
+    "url": "/tic-tac-toe/static/js/runtime-main.12138588.js"
+  }
+]);
